cefaa4409e4b40d4fa121ac7dd88ffc4cf63ad0b
BOUMAHDI Nabil <bylna4ever@free.fr>
2022-02-18 21:49:59 +0100

    add projet evaluation

---
+++ Contact/js/app.js
@@ -0,0 +1,89 @@
+// formulaire contact
+let button = document.getElementById('Envoyer');
+let nom = document.getElementById('nom');
+let prenom = document.getElementById('prenom');
+let email = document.getElementById('email');
+let telephone = document.getElementById('telephone');
+let message = document.getElementById('message');
+
+
+
+
+
+button.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  document.getElementById('error_nom').innerHTML = ''
+
+  if(nom.value === ""){
+    document.getElementById('error_nom').innerHTML = `
+    <p class='error'>champ vide !</p>
+  `
+  }
+  else if(nom.value.length < 5 || nom.value.length > 15){
+    document.getElementById('error_nom').innerHTML = `
+      <p class='error'>Le nom doit contenir au minimum 5 caractères !</p>
+    `
+  }
+
+});
+
+button.addEventListener('click', (event) => {
+    event.preventDefault();
+  
+    document.getElementById('error_prenom').innerHTML = ''
+  
+    if(nom.value === ""){
+      document.getElementById('error_prenom').innerHTML = `
+      <p class='error'>champ vide !</p>
+    `
+    }
+    else if(nom.value.length < 5 || nom.value.length > 15){
+      document.getElementById('prenom').innerHTML = `
+        <p class='error'>Le nom doit contenir au minimum 5 caractères !</p>
+      `
+    }
+  
+  });
+
+  button.addEventListener('click', (event) => {
+    event.preventDefault();
+  
+    document.getElementById('error_email').innerHTML = ''
+  
+    if(nom.value === ""){
+      document.getElementById('error_email').innerHTML = `
+      <p class='error'>champ vide !</p>
+    `
+    }
+    
+  
+  });
+
+  button.addEventListener('click', (event) => {
+    event.preventDefault();
+  
+    document.getElementById('error_telephone').innerHTML = ''
+  
+    if(nom.value === ""){
+      document.getElementById('error_telephone').innerHTML = `
+      <p class='error'>champ vide !</p>
+    `
+    }
+    
+  
+  });
+
+  button.addEventListener('click', (event) => {
+    event.preventDefault();
+  
+    document.getElementById('error_message').innerHTML = ''
+  
+    if(nom.value === ""){
+      document.getElementById('error_message').innerHTML = `
+      <p class='error'>champ vide !</p>
+    `
+    }
+    
+  
+  });
